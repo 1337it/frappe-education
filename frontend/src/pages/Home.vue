466090aa5a00833d737017c3ea71ccf6484fd4c4
1337it <91686534+1337it@users.noreply.github.com>
2024-09-28 19:54:54 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -19,6 +19,41 @@
           </div>
           <div>
 
+            <div class="flex gap-4" style="display:none;">
+              <div
+                v-for="section in infoFormat"
+                :key="section.section"
+                class="flex-1 flex flex-col gap-4"
+              >
+                <div v-for="field in section.fields" :key="field.label">
+                  <div
+                    class="flex items-center"
+                    v-if="field.label !== 'Address'"
+                  >
+                    <p class="w-1/2 text-sm text-gray-600">
+                      {{ field.label }}:&nbsp;
+                    </p>
+                    <p class="w-1/2 text-gray-900">{{ field.value }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="flex items-center" style="display:none;">
+              <p class="w-[32%] text-sm text-gray-600">
+                {{ infoFormat[0].fields[3].label }}:&nbsp;
+              </p>
+              <p class="w-full text-gray-900">
+                {{ infoFormat[0].fields[3].value }}
+              </p>
+            </div>
+          </div>
+
+          <div
+            class="flex items-center bg-gray-50 p-2 text-gray-600 text-sm rounded-md"
+           style="display:none;">
+            <FeatherIcon name="info" class="h-4 mr-2" />
+            In case of any incorrect details, please contact the school admin.
+          </div>
         </div>
       </div>
     </template>
