e353c839bb8dddf2befdd523a946da20b8c1b36b
1337it <91686534+1337it@users.noreply.github.com>
2024-10-03 21:49:35 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -2,11 +2,10 @@
   <iframe src='' id='frame' height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
 </template>
 <script>
-  window.addEventListener("load", (event) => {
-const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0];
+ setTimeout(() => {
+    const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0];
   const frame = document.getElementById("frame");
   frame.src = person;
-});
-
+  }, 200);
 
 </script>
