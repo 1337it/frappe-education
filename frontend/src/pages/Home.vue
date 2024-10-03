5c25b5f53136c021ad42e7b8646c02cca559bc2c
1337it <91686534+1337it@users.noreply.github.com>
2024-10-03 18:48:47 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <iframe src='/g/people' height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
+  <iframe src='' id='frame' height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
 </template>
 <script>
-
+const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0]);
+  const frame = document.getElementById("frame");
+  frame.src = person;
 </script>
