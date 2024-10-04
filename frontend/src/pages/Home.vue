b4fac53cd2c2651fb56f0f714e92c4977c98a785
1337it <91686534+1337it@users.noreply.github.com>
2024-10-04 08:55:23 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -2,6 +2,7 @@
   <iframe src='' id='frame' height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;animation:page-slide 0.3s;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
 </template>
 <script>
+  $('meta[name="theme-color"]').content = '#f6d365';
     setTimeout(() => {
     const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0];
   const frame = document.getElementById("frame");
