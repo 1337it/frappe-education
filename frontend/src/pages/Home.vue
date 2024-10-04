6a294a0a9a5b9ba05ab3a83eb5dd681328c3e4e6
1337it <91686534+1337it@users.noreply.github.com>
2024-10-04 08:38:46 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe src='' id='frame' height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;animation:page-slide 0.3s ease;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
+  <iframe src='' id='frame' height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;animation:page-slide 0.3s;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
 </template>
 <script>
     setTimeout(() => {
