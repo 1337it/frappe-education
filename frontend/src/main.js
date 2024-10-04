a5f14a751fb8f743d01c61c43e9365d738314c23
1337it <91686534+1337it@users.noreply.github.com>
2024-10-04 09:11:43 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -90,7 +90,7 @@ if(isMobile)  {
 }
 
 if (event.target.classList.contains("homeframe")) {
-	$('meta[name="theme-color"]').content = '#f6d365'
+document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#f6d365');
     setTimeout(() => {
     const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0];
   const frame = document.getElementById("frame");
