3db18ee985ad8e5f20a1b573e34c484dcd78094c
1337it <91686534+1337it@users.noreply.github.com>
2024-10-20 17:51:36 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -2,9 +2,11 @@
   <iframe src='' id='frame' height="100%" width="100%" style="padding: 0px;margin: 0px;animation: page-slide-down 0.2s;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
 </template>
 <script>
- window.onload = document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#f6d365');
+  document.addEventListener("DOMContentLoaded", function(event) {
 
- window.onload = document.getElementsByClassName('flex-1 flex flex-col h-full overflow-auto')[0].attributes.style.value = 'animation:page-slide-end-left 0.2s ease';
+document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#f6d365');
+
+document.getElementsByClassName('flex-1 flex flex-col h-full overflow-auto')[0].attributes.style.value = 'animation:page-slide-end-left 0.2s ease';
   });
 </script>
 <script setup>
