8dc3363b431438120d7553487b91709c5df37532
1337it <91686534+1337it@users.noreply.github.com>
2024-10-20 16:26:26 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -3,10 +3,12 @@
 </template>
 <script>
 document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#f6d365');
-  setTimeout(() => {
+
+document.getElementsByClassName('flex-1 flex flex-col h-full overflow-auto')[0].attributes.style.value = 'animation:page-slide-end-left 0.2s ease';
+
+</script>
+<script setup>
     const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0].split('%', 1)[0];
   const frame = document.getElementById("frame");
   frame.src = person;
-document.getElementsByClassName('flex-1 flex flex-col h-full overflow-auto')[0].attributes.style.value = 'animation:page-slide-end-left 0.2s ease';
-	}, 210);
 </script>
