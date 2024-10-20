d837fd81e3adb78d084c88ea0c44fab7ce9812f2
1337it <91686534+1337it@users.noreply.github.com>
2024-10-20 16:20:33 +0400

    Update Post.vue

--- frontend/src/pages/Post.vue
+++ frontend/src/pages/Post.vue
@@ -1,7 +1,7 @@
 <template>
-  <iframe src="/g/general/projects/3/discussions" height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;animation:page-slide 0.2s;" title="Posts"></iframe>
+  <iframe src="/g/general/projects/3/discussions" height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;animation:page-slide-end-right 0.2s ease;" title="Posts"></iframe>
 </template>
 <script>
-document.getElementsByClassName('flex-1 flex flex-col h-full overflow-auto')[0].attributes.style.value = 'animation:page-slide-end-right 0.2s ease';
+
 </script>
 
