457732271c106a14b0d8c700e1b6acc251c8b30c
1337it <91686534+1337it@users.noreply.github.com>
2024-10-20 17:21:53 +0400

    Update Fees.vue

--- frontend/src/pages/Fees.vue
+++ frontend/src/pages/Fees.vue
@@ -69,7 +69,9 @@
   </div>
 </template>
 <script>
+	$(document).on('app_ready', function() {
 	document.getElementsByClassName('flex-1 overflow-auto')[1].attributes.style.value = 'animation:page-slide-end-right 0.2s ease';
+	});
 </script>
 <script setup>
 import { useRoute } from 'vue-router'
