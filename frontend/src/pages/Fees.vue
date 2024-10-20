d5a639ca44cfff91d098489fb3dd58b2bb2d318c
1337it <91686534+1337it@users.noreply.github.com>
2024-10-20 16:05:16 +0400

    Update Fees.vue

--- frontend/src/pages/Fees.vue
+++ frontend/src/pages/Fees.vue
@@ -68,7 +68,9 @@
     <MissingData message="No Fees found" />
   </div>
 </template>
-
+<script>
+	document.getElementsByClassName('flex-1 flex flex-col h-full overflow-auto')[0].attributes.style.value = 'animation:page-slide-end-right 0.2s ease';
+</script>
 <script setup>
 import { useRoute } from 'vue-router'
 import {
