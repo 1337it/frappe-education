fd76c200fdbb71d5dcba2c8bd0d83277833f2560
1337it <91686534+1337it@users.noreply.github.com>
2024-10-24 10:51:17 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -5,6 +5,38 @@
 	<div id='postssection' class='tabsection' style=''>
   <iframe src="/g/general/projects/3/discussions" height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;animation:page-slide-end-right 0.2s ease;" title="Posts"></iframe>
 </div>
+	<div id='gradesection' class='tabsection' style=''>
+  <div v-if="grades.data?.length > 0">
+    <div class="px-5 py-4" style="animation:page-slide-end-right 0.2s ease">
+      <Dropdown class="mb-4" :options="allPrograms">
+        <template #default="{ open }">
+          <Button :label="selectedProgram">
+            <template #suffix>
+              <FeatherIcon
+                :name="open ? 'chevron-up' : 'chevron-down'"
+                class="h-4 text-gray-600"
+              />
+            </template>
+          </Button>
+        </template>
+      </Dropdown>
+      <ListView
+        class="h-[250px]"
+        :columns="tableData.columns"
+        :rows="tableData.rows"
+        :options="{
+          selectable: false,
+          showTooltip: false,
+          onRowClick: () => {},
+        }"
+        row-key="id"
+      />
+    </div>
+  </div>
+  <div v-else>
+    <MissingData message="No grades found" />
+  </div>
+		</div>
 <div id='feessection' class='tabsection' style=''>
   <div v-if="tableData.rows.length > 0" class="px-5 py-4" style="animation:page-slide-end-right 0.2s ease">
     <ListView
@@ -103,6 +135,21 @@ import MissingData from '@/components/MissingData.vue'
 import { createToast } from '@/utils'
 import Calendar from '@/components/Calendar.vue'
 import { studentStore } from '@/stores/student'
+	import {
+  Dropdown,
+  createListResource,
+} from 'frappe-ui'
+import { groupBy } from '@/utils'
+
+const { getCurrentProgram, getStudentInfo } = studentStore()
+
+let studentInfo = getStudentInfo().value
+let currentProgram = getCurrentProgram().value
+
+const allPrograms = ref([])
+const selectedProgram = ref('')
+
+const tableData = ref({
 const { getCurrentProgram, getStudentGroups } = studentStore()
 
 const programName = ref(getCurrentProgram()?.value?.program)
@@ -143,6 +190,89 @@ const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0].sp
    }, 100);
 
 
+  columns: [
+    {
+      label: 'Course',
+      key: 'course',
+    },
+    {
+      label: 'Batch',
+      key: 'batch',
+    },
+  ],
+  rows: [],
+})
+
+const student_programs = createResource({
+  url: 'education.education.api.get_student_programs',
+  makeParams() {
+    return {
+      // student: studentInfo.value?.name
+      student: studentInfo.name,
+    }
+  },
+  onSuccess: (response) => {
+    let programs = []
+    response.forEach((program) => {
+      programs.push({
+        label: program.program,
+        onClick: () => (selectedProgram.value = program.program),
+      })
+    })
+    selectedProgram.value = programs[programs.length - 1].label
+    allPrograms.value = programs
+  },
+  auto: true,
+})
+
+const grades = createListResource({
+  doctype: 'Assessment Result',
+  fields: [
+    'name',
+    'student_group',
+    'course',
+    'assessment_group',
+    'total_score',
+    'maximum_score',
+    'grade',
+  ],
+  filters: {
+    student: studentInfo.name,
+    program: currentProgram.program,
+    // student:"EDU-STU-2023-00005",
+    // program:"Comp Science"
+  },
+  transform: () => {},
+
+  onSuccess: (response) => {
+    let conductedExams = groupBy(response, (row) => row.assessment_group)
+    let exams = Object.keys(conductedExams)
+    updateColumns(exams)
+    let courses = groupBy(response, (row) => row.course)
+    Object.keys(courses).forEach((course) => {
+      let row = {}
+      row.course = course
+      row.batch = courses[course][0].student_group
+      exams.forEach((exam) => {
+        let examData = conductedExams[exam].find((row) => row.course === course)
+        row[exam] = examData
+          ? `${examData.total_score}/${examData.maximum_score}`
+          : '-'
+      })
+      tableData.value.rows.push(row)
+    })
+  },
+  auto: true,
+})
+
+const updateColumns = (exams) => {
+  exams.forEach((exam) => {
+    let col = {}
+    col.label = exam
+    col.key = exam
+    tableData.value.columns.push(col)
+  })
+}
 
 
 const { getStudentInfo } = studentStore()
