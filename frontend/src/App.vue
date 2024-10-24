79659ab5e1ead212e06a53fbc319963f95a8c054
1337it <91686534+1337it@users.noreply.github.com>
2024-10-24 17:45:13 +0400

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,4 @@
 <template>
-	<div id="JSMetroContainer" class="unselectable">
-		<div id="tempoUp" class="unselectable"></div>
-		<div id="tempoDown" class="unselectable"></div>
-		<div id="Beat" class="unselectable">0</div>
-		<div id="timeSignature" class="unselectable"></div>
-		<div id="TempoContainer" class="unselectable">
-			<span id="tempoDisplay" class="unselectable"></span>
-			<span id="tempoTag" class="unselectable">bpm</span>
-		</div>
-		<div id="tapBkg" class="unselectable"></div>
-		<div id="tap" class="unselectable">TAP</div>
-		<div id="power" class="unselectable"></div>
-		<div id="BellButton" class="unselectable"></div>
-		<audio id="metronomeTick_1" preload="auto">
-	     <source src="https://caljer1.github.io/MetronomeLite/sounds/click.mp3" type="audio/mp3" />
-<!--       <source src="https://caljer1.github.io/MetronomeLite/sounds/clave.mp3" type="audio/mp3" /> -->
-<!--       <source src="https://caljer1.github.io/MetronomeLite/sounds/clap.mp3" type="audio/mp3" /> -->
-<!--       <source src="https://caljer1.github.io/MetronomeLite/sounds/rimshot.mp3" type="audio/mp3" /> -->
-		</audio>
-		<audio id="metronomeBell" preload="auto">
-			<source src="https://caljer1.github.io/MetronomeLite/sounds/bell.mp3" type="audio/mp3" />
-		</audio>
-	</div>
     <div class="flex h-screen w-screen" style="background-size: 100vw auto;height:100vh !important;background-repeat: no-repeat;background-position: center;background-image:url('/files/image.jpg');">
       <div class="h-full border-r bg-gray-50">
         <Sidebar />
