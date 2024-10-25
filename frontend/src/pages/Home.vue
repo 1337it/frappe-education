ad7cac49a4474a82f8aa251e132b164c940fa96c
1337it <91686534+1337it@users.noreply.github.com>
2024-10-25 21:10:06 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -6,6 +6,8 @@
   <iframe src="/g/general/projects/3/discussions" height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;animation:page-slide-end-right 0.2s ease;" title="Posts"></iframe>
 </div>
 	<div id='gradesection' class='tabsection' style=''>
+		<button id="openmetro" class="" style=""></button>
+		<button id="opentanpura" class="" style=""></button>
   <div v-if="grades.data?.length > 0">
     <div class="px-5 py-4">
       <Dropdown class="mb-4" :options="allPrograms">
