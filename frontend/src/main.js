775b3b8b77400c37d8f43692afef32aeea20879f
1337it <91686534+1337it@users.noreply.github.com>
2024-10-26 14:01:24 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -136,17 +136,18 @@ var transtarget = 0;
 var movement = transtarget - transnow;
 	var currentsection = document.getElementsByClassName("tabsection active")[0].id;
 	document.getElementById(currentsection).classList.remove('active');
+	document.getElementById('homesection').classList.add('active');
 
 
-document.getElementById(currentsection).attributes.style.value = 'animation:page-slide-start-left 0.2s ease'; 
+document.getElementById(currentsection).attributes.style.value = 'animation:page-slide-start-right 0.2s ease'; 
 
 setTimeout(() => {
-	document.getElementById('homesection').attributes.style.value = 'animation:page-slide-end-right 0.2s ease;';
+	document.getElementById('homesection').attributes.style.value = 'animation:page-slide-end-left 0.2s ease;';
 		 }, 200);
 
 document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#f6d365');
 
-document.getElementById('homesection').classList.add('active');
+
 
 
 }
