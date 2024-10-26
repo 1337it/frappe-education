61ff517ed8074d5dac55bd86f13cdb0690d3d32e
1337it <91686534+1337it@users.noreply.github.com>
2024-10-26 15:01:44 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -140,9 +140,7 @@ if (event.target.classList.contains("homestab")) {
 
 document.getElementById(currentsection).attributes.style.value = 'animation:page-slide-start-right 0.2s ease'; 
 
-setTimeout(() => {
-	document.getElementById('homesection').attributes.style.value = 'animation:page-slide-end-left 0.2s ease;';
-		 }, 200);
+
 
 document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#f6d365');
 
