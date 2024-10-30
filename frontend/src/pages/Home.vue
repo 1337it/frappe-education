e8f51dc7edd6a2c495ff9f1ec4ab450307fe9830
1337it <91686534+1337it@users.noreply.github.com>
2024-10-30 23:22:00 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -139,17 +139,12 @@
 </template>
 <script type="text/javascript">
 	const data = {
-    labels: ["12am-3am", "3am-6pm", "6am-9am", "9am-12am",
-        "12pm-3pm", "3pm-6pm", "6pm-9pm", "9am-12am"
+    labels: ["Present", "Absent", "Remaining"
     ],
     datasets: [
         {
-            name: "Some Data", type: "bar",
-            values: [25, 40, 30, 35, 8, 52, 17, -4]
-        },
-        {
-            name: "Another Set", type: "line",
-            values: [25, 50, -10, 15, 18, 32, 27, 14]
+            name: "Some Data", type: "pie",
+            values: [2, 3, 3]
         }
     ]
 }
@@ -158,8 +153,8 @@ const chart = new Chart("#attendancepie", {  // or a DOM element,
                                             // new Chart() in case of ES6 module with above usage
     title: "My Awesome Chart",
     data: data,
-    type: 'axis-mixed', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
-    height: 250,
+    type: 'pie', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+    height: 10vh,
     colors: ['#7cd6fd', '#743ee2']
 })
 </script>
