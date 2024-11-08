4da4259b9bae48b0fb371ddbc3364b7adb141ba4
1337it <91686534+1337it@users.noreply.github.com>
2024-11-08 10:35:11 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -311,7 +311,11 @@ transport();
 
 });
 // set up the page
+
 function transport() {
+
+
+	
 var mapCanvas = document.getElementById('map_canvas');
 var geoBtn = document.querySelector('.enable');
 var revokeBtn = document.querySelector('.revoke');
@@ -351,9 +355,21 @@ var revealPosition = function(position) {
     map: map,
     title: markerTitle
   });
-	setTimeout(() => {
-	console.log(latlng);
-		 }, 100);
+
+	fetch(`https://app.kairaliartscentre.com:9999/hooks/getlocation`, {
+    method: 'GET',  }).then(r => 
+    r.text()) .then(r => {
+        const latcut = r.split("lat=", 100000);
+        const loncut = r.split("lon=", 100000);
+        const timecut = r.split("timestamp=", 100000);
+        var lat = latcut[latcut.length - 1].slice(0, 9);
+        var lon = loncut[loncut.length - 1].slice(0, 9);
+        var timestamp = timecut[timecut.length - 1].slice(0, 19);
+        root_element.getElementById('item').innerHTML = ''; 
+        root_element.getElementById('item').src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyBMTueLj6IEJA1eEePKjmA3tYNw-lnd3TQ&origin="+lat+","+lon+"&destination="+latlng+"&maptype=roadmap"
+        root_element.getElementById('time').innerText = 'Last Updated on '+moment(timestamp).add(4, 'hour').format('dddd, MMMM Do YYYY, h:mm:ss a');
+        console.log('Lat='+lat+' Lon='+lon+' Timestamp='+moment(timestamp).add(4, 'hour').format('dddd, MMMM Do YYYY, h:mm:ss a'));
+        })
 }
 
 // test for geolocation support, provide geolocation settings, determine location of the user's device
