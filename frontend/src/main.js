05e1ade1e5c33d57420ed5d78204e77f91cfe45e
1337it <91686534+1337it@users.noreply.github.com>
2024-11-08 12:36:45 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -355,6 +355,20 @@ var revealPosition = function(position) {
     map: map,
     title: markerTitle
   });
+fetch(`https://app.kairaliartscentre.com:9999/hooks/getlocation`, {
+    method: 'GET',  }).then(r => 
+    r.text()) .then(r => {
+        const latcut = r.split("lat=", 100000);
+        const loncut = r.split("lon=", 100000);
+        const timecut = r.split("timestamp=", 100000);
+        var lat = latcut[latcut.length - 1].slice(0, 9);
+        var lon = loncut[loncut.length - 1].slice(0, 9);
+        var timestamp = timecut[timecut.length - 1].slice(0, 19);
+        document.getElementById('item').innerHTML = ''; 
+        document.getElementById('item').src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyBMTueLj6IEJA1eEePKjmA3tYNw-lnd3TQ&origin="+lat+","+lon+"&destination="+latlng+"&maptype=roadmap"
+        document.getElementById('time').innerText = 'Last Updated on '+moment(timestamp).add(4, 'hour').format('dddd, MMMM Do YYYY, h:mm:ss a');
+        console.log('Lat='+lat+' Lon='+lon+' Timestamp='+moment(timestamp).add(4, 'hour').format('dddd, MMMM Do YYYY, h:mm:ss a'));
+        })
 
 
 
@@ -404,21 +418,7 @@ function report(state) {
 
 handlePermission();
 
-	fetch(`https://app.kairaliartscentre.com:9999/hooks/getlocation`, {
-    method: 'GET',  }).then(r => 
-    r.text()) .then(r => {
-        const latcut = r.split("lat=", 100000);
-        const loncut = r.split("lon=", 100000);
-        const timecut = r.split("timestamp=", 100000);
-        var lat = latcut[latcut.length - 1].slice(0, 9);
-        var lon = loncut[loncut.length - 1].slice(0, 9);
-        var timestamp = timecut[timecut.length - 1].slice(0, 19);
-        document.getElementById('item').innerHTML = ''; 
-        document.getElementById('item').src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyBMTueLj6IEJA1eEePKjmA3tYNw-lnd3TQ&origin="+lat+","+lon+"&destination="+latlng+"&maptype=roadmap"
-        document.getElementById('time').innerText = 'Last Updated on '+moment(timestamp).add(4, 'hour').format('dddd, MMMM Do YYYY, h:mm:ss a');
-        console.log('Lat='+lat+' Lon='+lon+' Timestamp='+moment(timestamp).add(4, 'hour').format('dddd, MMMM Do YYYY, h:mm:ss a'));
-        })
-
+	
 	
 }
   var JSmetronome = {
