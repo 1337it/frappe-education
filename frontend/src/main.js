b1373c0cd00b3c68eb05edaf4e194fa793b29157
1337it <91686534+1337it@users.noreply.github.com>
2024-11-09 13:56:25 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -179,6 +179,7 @@ document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#ff
 }
 
 if (event.target.classList.contains("scheduletab")) {
+	$('#schedulesection .w-full.h-full').append($('#frappeui-popper-root'));
 	var transnow = document.querySelectorAll('html')[0].attributes.style.value.split(';')[0].split(':')[1].split('%')[0];
 var transtarget = 200;
 var movement = transtarget - transnow;
