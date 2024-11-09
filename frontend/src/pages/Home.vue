428e9decf9629e368e7a64184930a1616975542d
1337it <91686534+1337it@users.noreply.github.com>
2024-11-09 12:16:26 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -29,7 +29,7 @@
 <button class="revoke" style="display:none">Revoke permission</button>
 </div>
 <div id="map"></div>
-<iframe id="item" width="100%" height="100%" frameborder="0" style="border: 1px solid white;width: 90vw;left: 5vw;position: fixed;top: 5vw;height: 66vh;box-shadow: #00000026 0 5px 15px;" referrerpolicy="no-referrer-when-downgrade" src allowfullscreen></iframe>
+<iframe id="item" width="100%" height="100%" frameborder="0" style="border-radius: 15px;border: 1px solid white;width: 90vw;left: 5vw;position: fixed;top: 5vw;height: 66vh;box-shadow: #00000026 0 5px 15px;" referrerpolicy="no-referrer-when-downgrade" src allowfullscreen></iframe>
 		</div>
 	<div id='postssection' class='tabsection' style='opacity:1;'>
   <iframe src="/g/general/projects/3/discussions" height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
