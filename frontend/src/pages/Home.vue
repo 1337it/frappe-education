eaaa01f746ee031a7e62cf5dc6bccb9e9c4c6e56
1337it <91686534+1337it@users.noreply.github.com>
2024-11-16 17:48:11 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -22,14 +22,6 @@
 		</div>
 	    </div>
 	<div id='transportsection' class='tabsection' style='opacity:1;'>
-		<script> (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
-    key: "AIzaSyBMTueLj6IEJA1eEePKjmA3tYNw-lnd3TQ",
-    v: "weekly",
-    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
-    // Add other bootstrap parameters as needed, using camel case.
-  });
-
-		</script>
 		<div id="vehicledetails" style="position:fixed;bottom:100px;width:80vw;left:10vw;backdrop-filter: blur(10px);background-color: rgba(255, 255, 255, 0.62);box-shadow: #00000026 0 5px 15px;">
 			<p id="time"></p>
 			<p id="seen"></p>
@@ -321,7 +313,7 @@ const scheduleResource = createResource({
   document.querySelector('meta[name="theme-color"]').setAttribute('content',  '#f6d365');
 
 setTimeout(() => {
-const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0].split('%', 1)[0];
+const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0].replace(/\%40/g, "@");
   const frame = document.getElementById("frame");
   frame.src = person;
    }, 100);
