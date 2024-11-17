8380379cf71b891f1a41e64c6d704ea7fb9d98f6
1337it <91686534+1337it@users.noreply.github.com>
2024-11-17 19:44:41 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -32,7 +32,7 @@
 <iframe id="item" width="100%" height="100%" frameborder="0" style="border-radius: 15px;border: 1px solid white;width: 90vw;left: 5vw;position: fixed;top: 5vw;height: 66vh;box-shadow: #00000026 0 5px 15px;" referrerpolicy="no-referrer-when-downgrade" src allowfullscreen></iframe>
 		</div>
 	<div id='postssection' class='tabsection' style='opacity:1;'>
-  <iframe src="/g/general/projects/3/discussions" height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
+  <iframe src="/g/general/projects/12/discussions" height="100%" width="100%" style="padding: 0px;margin: 0px;z-index: 9;width: 100vw;position: absolute;left: 0;height:100vh;top:0;" title="Posts"></iframe>
 </div>
 	<div id='gradesection' class='tabsection' style='opacity:1;'>
   <div v-if="grades.data?.length > 0">
