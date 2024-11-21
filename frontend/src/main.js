ebc9fd5e9e3ae9f2745b344e7d24b526aeb442b4
1337it <91686534+1337it@users.noreply.github.com>
2024-11-21 07:36:36 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -462,8 +462,8 @@ function getAccel(){
             window.addEventListener('deviceorientation',(event) => {
                 // Expose each orientation angle in a more readable way
 
-                var frontToBack_degrees = event.beta;
-		    var sidetoside_degrees = event.gamma;
+                var frontToBack_degrees = event.beta * 3;
+		    var sidetoside_degrees = event.gamma * 3;
                
                 
                 // Update velocity according to how tilted the phone is
