1fb2480a862dc11ba4f46597f01a015078368037
1337it <91686534+1337it@users.noreply.github.com>
2024-11-25 02:42:47 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -330,8 +330,6 @@ revokeBtn.onclick = function() {
   revokePermission();
 }
 
-// draw the google map, or not
-
   
 var positionDenied = function() {
   geoBtn.style.display = 'inline';
@@ -469,11 +467,65 @@ function getAccel(){
                 
                 // Update velocity according to how tilted the phone is
                 // Since phones are narrower than they are long, double the increase to the x velocity
-               
-                
-                var dot = document.getElementsByClassName("mainbg")[0];
-             
-		document.querySelector('#bgpart .card').attributes.style.value = "--posy:50%; --posx: " + vertical + "%; --hyp: 0; --o: 1; --mx: 0%; --my: 0%;";  
+        const $cards = $(".card");
+const $style = $(".hover");
+
+$cards.on("deviceorientation", function(e) {
+  var $card = $(this);
+  var l = e.offsetX;
+  var t = e.offsetY;
+  var h = $card.height();
+  var w = $card.width();
+  var lp = Math.abs(Math.floor(100 / w * l)-100);
+  var tp = Math.abs(Math.floor(100 / h * t)-100);
+  var bg = `background-position: ${lp}% ${tp}%;`
+  var style = `.card.active:before { ${bg} }`
+  $cards.removeClass("active");
+  $card.addClass("active");
+  $style.html(style);
+}).on("mouseout", function() {
+  $cards.removeClass("active");
+});
+
+//
+// 3d hover magic
+//
+const resetTransform = (el, perspective = 800) =>
+  (el.style.transform = `translate3d(0%, 0%, -${perspective /
+    2}px) rotateX(0deg) rotateY(0deg)`);
+
+const onMove = (ev, el) => {
+  const { pageX, pageY } = ev;
+  const { offsetWidth, offsetHeight } = el;
+  const { left, top } = el.getBoundingClientRect();
+
+  const cardX = left + offsetWidth / 2;
+  const cardY = top + offsetHeight / 2;
+
+  const angle = 25;
+  const rotX = (cardY - pageY) / angle;
+  const rotY = (cardX - pageX) / -angle;
+
+  el.style.transform = `translate3d(0%, 0%, 0) rotateX(${horizontal}deg) rotateY(${vertical}deg)`;
+};
+
+const perspective =
+    getComputedStyle($cards[0].parentElement)
+      .getPropertyValue("perspective")
+      .replace("px", "") || 800;
+
+const onCardMove = ev => onMove(ev, ev.target);
+const onHover = ev => ev.target.addEventListener("mousemove", onCardMove);
+const onOut = ev => {
+  resetTransform(ev.target, perspective); // reset card
+  ev.target.removeEventListener("mousemove", onCardMove);
+};
+
+// setup cards interaction
+[...$cards.toArray()].forEach(card => {
+  card.addEventListener("click", onHover);
+  card.addEventListener("click", onOut);
+});
 
 
 	
@@ -482,6 +534,8 @@ function getAccel(){
         }
     });
 }
+
+
 
 
   var JSmetronome = {
