06fa5309b35db77a0f7c3940d638ec74b65003b2
1337it <91686534+1337it@users.noreply.github.com>
2024-11-25 06:15:46 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -484,11 +484,10 @@ const $style = document.querySelector(".hover");
   var w = window.getComputedStyle(document.querySelector(".card.active")).width.split('px')[0];
   var lp = Math.abs(Math.floor(100 / w * l)-100);
   var tp = Math.abs(Math.floor(100 / h * t)-100);
-  var bg = 'background-position: ${lp}% ${tp}%;';
-  var style = '.card.active:before { ${bg} }';
-  $cards.removeClass("active");
-  $card.addClass("active");
-  $style.html(style);
+  var bg = `background-position: ${lp}% ${tp}%;`;
+  var style = `.card.active:before { ${bg} }`;
+
+  $style.innerHTML = style;
 
 //
 // 3d hover magic
