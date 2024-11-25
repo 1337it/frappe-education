6496330f28009cce10f698be52c2277cd076b270
1337it <91686534+1337it@users.noreply.github.com>
2024-11-25 07:16:01 +0400

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -484,7 +484,7 @@ const $style = document.querySelector(".hover");
   var w = window.getComputedStyle(document.querySelector(".card.active")).width.split('px')[0];
   var lp = Math.abs(Math.floor(100 / w * l)-100);
   var tp = Math.abs(Math.floor(100 / h * t)-100);
-  var bg = `background-position: ${t + 50}% ${l + 50%};`;
+  var bg = `background-position: ${t + 50}% ${l + 50}%;`;
   var style = `.card.active:before { ${bg} }`;
 
   $style.innerHTML = style;
