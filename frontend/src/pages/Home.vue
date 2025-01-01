222005fb2145a62716cde3ca51daf9f573e66a09
1337it <91686534+1337it@users.noreply.github.com>
2025-01-02 00:00:55 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -391,7 +391,7 @@ const updateColumns = (exams) => {
 const streaks = createResource({
   url: 'education.api.student.get_streak_for_student',
   params: {
-    student_id: studentInfo.id,
+    student_id: studentInfo.name,
   },
 onSuccess: (response) => {
 
