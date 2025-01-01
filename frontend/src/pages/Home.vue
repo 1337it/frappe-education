e44941334c43f2b00dfc78aa08c5660a774703a7
1337it <91686534+1337it@users.noreply.github.com>
2025-01-01 20:59:50 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -388,8 +388,21 @@ const updateColumns = (exams) => {
   })
 }
 
-
-
+const streaks = createResource({
+  url: 'education.api.student.get_streak_for_student',
+  params: {
+    student_id: studentInfo.id,
+  },
+onSuccess: (response) => {
+if (response.message) {
+                        document.getElementById("current-streak").innerText = response.message.current_streak;
+                        document.getElementById("highest-streak").innerText = response.message.highest_streak;
+                    } else {
+                        document.getElementById("current-streak").innerText = "0";
+                        document.getElementById("highest-streak").innerText = "0";
+                    }
+}
+});
 
 const feesResource = createResource({
   url: 'education.education.api.get_student_invoices',
