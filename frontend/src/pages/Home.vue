7edf78e4b57dc4fbb861675fcb28ec69fe4d1248
1337it <91686534+1337it@users.noreply.github.com>
2025-01-07 22:46:55 +0400

    Update Home.vue

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -314,7 +314,7 @@ setTimeout(() => {
 const person = '/g/people/'+document.cookie.split('=', 4)[3].split(';', 1)[0].replace('%40', '@');
   const frame = document.getElementById("frame");
   frame.src = person;
-	document.getElementsByClassName(' p-5 h-full')[0].append(document.getElementById('frappeui-popper-root'));
+	
    }, 100);
 
 
